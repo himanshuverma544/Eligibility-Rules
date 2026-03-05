3165154ffb6ff2b29e99e686a3f950dd34aeeb5b
Himanshu Verma <himanshuverma544@gmail.com>
2026-03-05 07:01:29 +0000

    upd: extended functionality of input field to won't let enter the negative and not required values

--- src/components/layouts/Input.jsx
+++ src/components/layouts/Input.jsx
@@ -1,14 +1,63 @@
-const Input = ({ className = "", type = "text", placeholder = "",  ...props }) => {
+import { useState } from "react";
+
+
+const Input = ({ className = "", type = "text", placeholder = "", ...props }) => {
+
+  const [inputValue, setInputValue] = useState("");
+
+
+  const handleInputProtocols = event => {
+
+    let value = event.target.value;
+
+    switch (type) {
+
+      case "number":
+
+        if (event.type === "keydown") {
+          if (["-", "e", "E"].includes(event.key)) {
+            event.preventDefault();
+            return;
+          }
+        }
+
+        if (event.type === "paste") {
+          const pasted = (event.clipboardData || window.clipboardData).getData("text");
+          if (/[-eE]/.test(pasted)) {
+            event.preventDefault();
+            return;
+          }
+        }
+
+        if (event.type === "change") {
+          const positiveNumsRegex = /^\d*\.?\d*$/;
+          if (positiveNumsRegex.test(value)) {
+            setInputValue(value);
+          }
+        }
+
+        break;
+
+      default:
+        setInputValue(value);
+        break;
+    }
+  }
+
 
   return (
     <input
       className={`input-${type} px-4 border rounded text-sm ${className}`}
       type={type}
       placeholder={placeholder}
+      value={inputValue}
+      onChange={handleInputProtocols}
+      onKeyDown={handleInputProtocols}
+      onPaste={handleInputProtocols}
       {...props}
     />
   );
-}
+};
 
 
 export default Input;
